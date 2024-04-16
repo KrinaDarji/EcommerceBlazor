53d80ab914e99a7e65ba3da25276753ab9656747
Krina Darji <krina.d@simformsolutions.com>
2024-04-16 10:09:51 +0530

    Added database and entity framework

---
+++ Ecommerce_DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Ecommerce_DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce_DataAccess;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+    public DbSet<Category> Categories { get; set; }
+}
