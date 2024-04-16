e1077b4ada7566364a84b4e4a33487ce670619d4
Krina Darji <krina.d@simformsolutions.com>
2024-04-16 11:54:51 +0530

    Added validation for the create category

--- Ecommerce_Models/CategoryDTO.cs
+++ Ecommerce_Models/CategoryDTO.cs
@@ -1,7 +1,10 @@
-﻿namespace Ecommerce_Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce_Models;
 
 public class CategoryDTO
 {
     public int Id { get; set; }
+    [Required]
     public string Name { get; set; }    
 }
